69efe51dc6da85335718e3aadcda32f2c295e9b4
rosemin928 <rosemin928@naver.com>
2025-12-01 22:46:58 +0900

    refactor: 수동 DI 컨테이너 코드를 Hilt로 마이그레이션
    기존에 사용하던 수동 DI 컨테이너 설정(`DiBindings.kt`, `ContainerBuilder`)을 제거하고, Dagger Hilt를 사용하도록 `ShoppingApplication`을 수정했습니다. 이에 따라 관련 없는 `Container` 초기화 및 테스트용 재정의 로직도 삭제되었습니다.

--- app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
+++ app/src/main/java/woowacourse/shopping/ShoppingApplication.kt
@@ -1,26 +1,7 @@
 package woowacourse.shopping
 
 import android.app.Application
-import androidx.annotation.VisibleForTesting
 import dagger.hilt.android.HiltAndroidApp
-import woowacourse.bibi.di.core.Container
-import woowacourse.bibi.di.core.ContainerBuilder
-import woowacourse.shopping.di.installAllBindings
 
 @HiltAndroidApp
-class ShoppingApplication : Application() {
-    lateinit var container: Container
-        private set
-
-    override fun onCreate() {
-        super.onCreate()
-        val builder = ContainerBuilder()
-        installAllBindings(builder, this)
-        container = builder.build()
-    }
-
-    @VisibleForTesting
-    fun overrideContainerForTest(testContainer: Container) {
-        container = testContainer
-    }
-}
+class ShoppingApplication : Application()
